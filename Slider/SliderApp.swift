15a46a9da6545432a13e65f8168533cab246b349
Alexander Römer <alex@roemer.design>
2020-09-22 15:48:21 +0200

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Alexander Römer on 22.09.20.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
